aa58670b6462f08b1ac17b703ecb433098f2030e
taras-stet-din0 <tabasska2806@gmail.com>
2026-05-30 10:21:16 +0200

    commit 4 local storage

--- js/cart.js
+++ js/cart.js
@@ -1,67 +1,91 @@
-const cartWrapper = document.querySelector('.cart-wrapper');
+const cartWrapper = document.querySelector('.cart-wrapper'); 
+let cartLocal = [];
 
-window.addEventListener('click', function(event){
-    if(event.target.hasAttribute('data-cart')){
-        const cart = event.target.closest('.card');
- 
-        const productInfo = {
-            id: cart.dataset.id,
-            imgSrc: cart.querySelector('.product-img').getAttribute('src'),
-            title: cart.querySelector('.item-title').innerText,
-            itemsInBox: cart.querySelector('[data-items-in-box]').innerText,
-            weight: cart.querySelector('.price__weight').innerText,
-            price: cart.querySelector('.price__currency').innerText,
-            counter: cart.querySelector('[data-counter]').innerText,
-        }
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cartLocal));
+}
 
-        const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
-        console.log(productInfo.id);
-        console.log(cartWrapper);
-        
-        
-        if(itemInCart){
-            const counterElement = itemInCart.querySelector('[data-counter]');
-            counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
-        } else {
-            const cartItemHtml = `
-        <div class='cart-item' data-id="${productInfo.id}"> 
+function loadCart() {
+    const savedCart = localStorage.getItem('cart');
+
+    if (savedCart) {
+        cartLocal = JSON.parse(savedCart);
+    }
+}
+
+
+function renderCart() {
+
+    cartWrapper.innerHTML = '';
+
+    cartLocal.forEach(product => {
+
+        const cartItemHtml = `
+        <div class="cart-item" data-id="${product.id}">
             <div class="cart-item__top">
+
                 <div class="cart-item__img">
-                    <img src="${productInfo.imgSrc}" alt="">
+                    <img src="${product.imgSrc}" alt="">
                 </div>
+
                 <div class="cart-item__desc">
-                    <div class="cart-item__title">${productInfo.title}</div>
-                    <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
-                    
-                    <!-- cart-item__details -->
+
+                    <div class="cart-item__title">
+                        ${product.title}
+                    </div>
+
+                    <div class="cart-item__weight">
+                        ${product.itemsInBox} / ${product.weight}
+                    </div>
+
                     <div class="cart-item__details">
 
-                        <div class="items items--small counter-wwrapper">
-                            <div class="items__control" data-action="minus">-</div>
-                            <div class="items__current" data-counter=""> ${productInfo.counter}</div>
-                        </div>
-                        <div class="items__control" data-action="plus">+</div>
+                        <div class="items items--small counter-wrapper">
+
+                            <div
+                                class="items__control"
+                                data-action="minus">
+                                -
+                            </div>
+
+                            <div
+                                class="items__current"
+                                data-counter>
+                                ${product.counter}
+                            </div>
+
+                            <div
+                                class="items__control"
+                                data-action="plus">
+                                +
+                            </div>
+
                         </div>
 
                         <div class="price">
-                            <div class="price__currency">${productInfo.price}
+                            <div class="price__currency">
+                                ${product.price}
+                            </div>
                         </div>
-        
+
                     </div>
-                    <!-- //cart-item__details -->
-        
                 </div>
             </div>
-        </div>   
+        </div>
         `;
-        cartWrapper.insertAdjacentHTML('beforeend', cartItemHtml);  
-        } 
-        function toggleCartStatus() {
+
+        cartWrapper.insertAdjacentHTML('beforeend',cartItemHtml);
+    });
+
+    toggleCartStatus();
+};
+
+function toggleCartStatus() {
         const cartEmpty = document.querySelector('[data-cart-empty]');
         const cartTotal = document.querySelector('.cart-total');
         const orderForm = document.querySelector('#order-form');
 
-        if (cartWrapper.querySelectorAll('.cart-item').length > 0) {
+        if (cartLocal.length > 0) {
             cartEmpty.classList.add('none');
             cartTotal.classList.remove('none');
             orderForm.classList.remove('none');
@@ -72,19 +96,44 @@ window.addEventListener('click', function(event){
         }
         
         let totalPrice = 0;
-        cartWrapper.querySelectorAll('.cart-item').forEach(function(item) {
-            const counter = item.querySelector('[data-counter]').innerText;
-            console.log(item.querySelector('.price__currency'));
-            const priceOneItem = item.querySelector('.price__currency').innerText;
-            const price = parseInt(counter) * parseInt(priceOneItem);
-            totalPrice = totalPrice + price;
-            cartTotal.querySelector('.total-price').innerText = totalPrice; 
-        })
+
+        cartLocal.forEach(item =>{
+            totalPrice += parseInt(item.price) * parseInt(item.counter);
+
+        });
+        document.querySelector('.total-price').innerText = totalPrice;
+};
+
+window.addEventListener('click', function(event){
+    if(event.target.hasAttribute('data-cart')){
+
+        const cart = event.target.closest('.card');
+
+        const productInfo = {
+            id: cart.dataset.id,
+            imgSrc: cart.querySelector('.product-img').getAttribute('src'),
+            title: cart.querySelector('.item-title').innerText,
+            itemsInBox: cart.querySelector('[data-items-in-box]').innerText,
+            weight: cart.querySelector('.price__weight').innerText,
+            price: cart.querySelector('.price__currency').innerText,
+            counter: cart.querySelector('[data-counter]').innerText,
+        };
+
+
+        const itemInCart =
+            cartLocal.find(item => item.id === productInfo.id);
+        if(itemInCart){
+            itemInCart.counter = parseInt(itemInCart.counter) + parseInt(productInfo.counter);
+        } else {
+            cartLocal.push(productInfo);
+        }
+        saveCart();
+        renderCart();
+
+        cart.querySelector('[data-counter]').innerText = '1';
     }
-        cart.querySelector('[data-counter]').innerText = '1';   
+});
 
-        toggleCartStatus()
-    }
 
-})
-
+loadCart();
+renderCart();
